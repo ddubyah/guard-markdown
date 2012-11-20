d3f24eda7acce535ffc93ce1dbda044ff1a253ec
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-11-20 16:21:39 -0200

    color and environment configuration on helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'guard/markdown'
-ENV["GUARD_ENV"] = 'test'
+RSpec.configure do |config|
+  config.color_enabled = true
+
+  config.before(:each) do
+    ENV["GUARD_ENV"] = 'test'
+  end
+end
