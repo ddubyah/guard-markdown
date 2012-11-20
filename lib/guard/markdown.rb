296433f33c0c0063c9b945535ccb2fdc0c9b6b2a
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-11-20 15:55:25 -0200

    no more trailing spaces

--- lib/guard/markdown.rb
+++ lib/guard/markdown.rb
@@ -3,31 +3,31 @@ require 'guard/guard'
 require 'guard/watcher'
 require 'kramdown'
 
-module Guard  
+module Guard
   class Markdown < Guard
     # Your code goes here...
     attr_reader :kram_ops
     def initialize(watchers=[], options={})
-      super              
+      super
       @options = {
         :convert_on_start => true,
         :dry_run          => false
       }.update(options)
       @kram_ops = { :input => "kramdown", :output => "html" }
       @kram_ops.update(@options[:kram_ops]) if @options[:kram_ops]
-    end         
-    
+    end
+
     def start
       UI.info("Guard::Markdown has started watching your files")
       run_all if @options[:convert_on_start]
     end
-    
+
     def run_all
       files = Dir.glob("**/*.*")
-      targets = Watcher.match_files(self, files)  
+      targets = Watcher.match_files(self, files)
       run_on_change targets
     end
-    
+
     # Called on file(s) modifications
     # TODO  - this method does far too much. Must refactor to allow
     #       - for better testing
@@ -35,21 +35,21 @@ module Guard
       paths.each do |path|
         input, output, template = path.split("|")
         info = "#{input} >> #{output}"
-        info = "#{info} via #{template}" unless template.nil? 
-        UI.info info 
+        info = "#{info} via #{template}" unless template.nil?
+        UI.info info
         unless @options[:dry_run]
-          source = File.open(input,"rb").read                        
-                                            
+          source = File.open(input,"rb").read
+
           # make sure directory path exists
           reg = /(.+\/).+\.\w+/i
           target_path = output.gsub(reg,"\\1")
           FileUtils.mkpath target_path unless target_path.empty?
-          
+
           @kram_ops.update({ :template => template }) unless template.nil?
-          
+
           doc = Kramdown::Document.new(source, @kram_ops).to_html
-                   
-        
+
+
           File.open(output, "w") do |f|
             f.write(doc)
           end
